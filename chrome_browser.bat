5909fc706d4c2ec4047b4346a4d3a056f43f8ce6
nikitasbarge <nikitasbarge@gmail.com>
2023-08-02 22:39:22 +0530

    demo file added

--- chrome_browser.bat
+++ chrome_browser.bat
@@ -1,1 +1,1 @@
-pytest -v -s -n=2 --html=Html_Reports/my_html_report1.html --alluredir="D:\automation testing\PYCHARM\Credkart_My_Project\allure.results"
+pytest -v -s -n=2 --html=Html_Reports/my_html_report1.html --alluredir="D:\automation testing\PYCHARM\Credkart_My_Project\allure-results"
